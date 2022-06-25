7cbbb24c98fe943e4a1bb61e8cea6a4ea80c3232
AMIT AMRUTIYA <amitamrutiya2210@gmail.com>
2022-06-25 10:38:12 +0530

    whatsup_UI

---
+++ android/app/src/main/kotlin/com/example/whatup_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whatup_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('WhatsApp',style: TextStyle(color: Colors.white),),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
