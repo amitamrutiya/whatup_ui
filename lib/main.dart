7371fe85f0e422e9a30c5edf5660a7454a6039cb
AMIT AMRUTIYA <amitamrutiya2210@gmail.com>
2022-06-25 11:01:36 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -11,64 +11,70 @@ class MyApp extends StatelessWidget {
         length: 4,
         initialIndex: 1,
         child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: const Color(0xff004C58),
-              title: const Text(
-                'WhatsApp',
-                style: TextStyle(color: Colors.white),
-              ),
-              actions: const [
-                Icon(Icons.search),
-                SizedBox(width: 14),
-                Icon(Icons.more_vert),
-                SizedBox(width: 4)
-              ],
-              bottom: const TabBar(
-                  indicatorColor: Colors.white,
-                  labelPadding: EdgeInsets.only(bottom: 10),
-                  tabs: [
-                    Icon(
-                      Icons.camera_alt,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "CHATS",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Text(
-                      "STATUS",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Text(
-                      "CALLS",
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ]),
+          appBar: AppBar(
+            backgroundColor: const Color(0xff004C58),
+            title: const Text(
+              'WhatsApp',
+              style: TextStyle(color: Colors.white),
             ),
-            body: TabBarView(
-              children: [
-                Container(
-                  child: const Center(
-                    child: Text("Camera Screen"),
+            actions: const [
+              Icon(Icons.search),
+              SizedBox(width: 14),
+              Icon(Icons.more_vert),
+              SizedBox(width: 4)
+            ],
+            bottom: const TabBar(
+                indicatorColor: Colors.white,
+                labelPadding: EdgeInsets.only(bottom: 10),
+                tabs: [
+                  Icon(
+                    Icons.camera_alt,
+                    color: Colors.white,
                   ),
-                ),
-                Container(
-                  child: const Center(
-                    child: Text("Chat Screen"),
+                  Text(
+                    "CHATS",
+                    style: TextStyle(color: Colors.white),
                   ),
-                ),
-                Container(
-                  child: const Center(
-                    child: Text("Status Screen"),
+                  Text(
+                    "STATUS",
+                    style: TextStyle(color: Colors.white),
                   ),
+                  Text(
+                    "CALLS",
+                    style: TextStyle(color: Colors.white),
+                  )
+                ]),
+          ),
+          body: TabBarView(
+            children: [
+              Container(
+                child: const Center(
+                  child: Text("Camera Screen"),
                 ),
-                Container(
-                  child: const Center(
-                    child: Text("Call Screen"),
-                  ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text("Chat Screen"),
                 ),
-              ],
-            )),
+              ),
+              Container(
+                child: const Center(
+                  child: Text("Status Screen"),
+                ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text("Call Screen"),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: const Color(0xff13955F),
+            child: const Icon(Icons.message),
+          ),
+        ),
       ),
     );
   }
